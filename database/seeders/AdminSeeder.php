d49d0e217d4c744c2b901c4fc082ec28d293e36a
BadreddineZatout <hb_zatout@esi.dz>
2025-05-08 15:31:05 +0100

    init

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@logisco.dz',
+            'password' => Hash::make('password'),
+        ]);
+
+        $user->assignRole('super_admin');
+    }
+}
